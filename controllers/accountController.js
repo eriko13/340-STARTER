b070d261c5922a373385bc8ffd2c54de8365b500
eriko13 <erikgerman917@gmail.com>
2025-06-17 12:30:34 -0700

    c

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -202,8 +202,21 @@ accountController.accountLogin = async function (req, res) {
       return
     }
 
+    // Hash the password before comparing
+    let hashedPassword
+    try {
+      // regular password and cost (salt is generated automatically)
+      hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+      req.flash("notice", 'Sorry, there was an error processing the login.')
+      res.status(500).render("account/login", {
+        title: "Login",
+        nav,
+        errors: null,
+      })
+    }
     // Compare provided password with hashed password from database
-    const passwordMatch = await bcrypt.compare(account_password, accountData.account_password)
+    const passwordMatch = await bcrypt.compare(hashedPassword, accountData.account_password)
 
     if (passwordMatch) {
       delete accountData.account_password
@@ -241,12 +254,24 @@ accountController.accountLogin = async function (req, res) {
 accountController.registerAccount = async function (req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
-
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
